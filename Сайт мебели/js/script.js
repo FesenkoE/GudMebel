eca7bcf292762b7c7a0fab9f8ba76140dae4bcae
Evgeniy Fesenko <fesenkoe@ukr.net>
2017-05-30 01:18:27 +0300

    add fotter
    add footer, add button "toTop", changed paddin by chapters, changed
    adaptive products, delete hover to top sellinfg prodicts in lmenu

--- Сайт мебели/js/script.js
+++ Сайт мебели/js/script.js
@@ -26,3 +26,17 @@ $(".container .tab").click(function() {
 	$(".container .tab").removeClass("active").eq($(this).index()).addClass("active");
 	$(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
+
+/*toTop*/
+$(function() { 
+$(window).scroll(function() { 
+if($(this).scrollTop() != 0) { 
+$('.toTop').fadeIn(); 
+} else { 
+$('.toTop').fadeOut(); 
+} 
+}); 
+$('.toTop').click(function() { 
+$('body,html').animate({scrollTop:0},800); 
+}); 
+});
